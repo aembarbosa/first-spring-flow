f5c548f29d096c19a7e6468e4a1d8071b2bb5aaa
Alyne Emmily <aembarbosa@outlook.com>
2023-12-14 17:45:24 -0300

    Exception handling - delete

---
+++ FirstSpringFlow/src/main/java/com/firstspringflow/FirstSpringFlow/services/exceptions/DatabaseException.java
@@ -0,0 +1,9 @@
+package com.firstspringflow.FirstSpringFlow.services.exceptions;
+
+public class DatabaseException extends RuntimeException {
+
+    public DatabaseException(String msg) {
+        super(msg);
+    }
+
+}
