f0ccd7fad64003abebf9be1d48cc26660e295c05
Alyne Emmily <aembarbosa@outlook.com>
2023-12-14 16:57:36 -0300

    Exception handling - findById

---
+++ FirstSpringFlow/src/main/java/com/firstspringflow/FirstSpringFlow/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,24 @@
+package com.firstspringflow.FirstSpringFlow.resources.exceptions;
+
+import com.firstspringflow.FirstSpringFlow.services.exceptions.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+
+    // Annotation que vai interceptar a requisicao que deu excecao
+    @ExceptionHandler(ResourceNotFoundException.class)
+    public ResponseEntity<StandardError> resourceNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+        String error = "Resource not found";
+        HttpStatus status = HttpStatus.NOT_FOUND;
+        StandardError err = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+        return ResponseEntity.status(status).body(err);
+    }
+
+}
--- FirstSpringFlow/src/main/java/com/firstspringflow/FirstSpringFlow/services/UserService.java
+++ FirstSpringFlow/src/main/java/com/firstspringflow/FirstSpringFlow/services/UserService.java
@@ -2,6 +2,7 @@ package com.firstspringflow.FirstSpringFlow.services;
 
 import com.firstspringflow.FirstSpringFlow.entities.User;
 import com.firstspringflow.FirstSpringFlow.repositories.UserRepository;
+import com.firstspringflow.FirstSpringFlow.services.exceptions.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +21,7 @@ public class UserService {
 
     public User findById(Long id) {
        Optional<User> obj = repository.findById(id);
-       return obj.get();
+       return obj.orElseThrow(() -> new ResourceNotFoundException(id));
     }
 
     // Inserir um usuario no banco de dados
