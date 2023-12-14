8bfc9c06c43ba3efe9bf06da3c2639db2cba6f92
Alyne Emmily <aembarbosa@outlook.com>
2023-12-14 11:15:40 -0300

    Subtotal and Total methods

--- FirstSpringFlow/src/main/java/com/firstspringflow/FirstSpringFlow/entities/Payment.java
+++ FirstSpringFlow/src/main/java/com/firstspringflow/FirstSpringFlow/entities/Payment.java
@@ -1,5 +1,6 @@
 package com.firstspringflow.FirstSpringFlow.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -15,6 +16,7 @@ public class Payment implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonIgnore
     // Esse mapeamento ocorre no lado dependente
     @OneToOne
     @MapsId
